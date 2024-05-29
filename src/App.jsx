39769524424f6edfb481d9b93ae3d2ced8a45c50
farhank15 <afarhank22@gmail.com>
2024-05-29 11:19:10 +0700

    fix Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "@pages/homePages";
 import Katalog from "@pages/katalogPages";
 import Wilayah from "@pages/wilayahPages";
@@ -10,18 +10,16 @@ import Signup from "@pages/signupPages";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:id" element={<Home />} />
-        <Route path="/katalog" element={<Katalog />} />
-        <Route path="/wilayah" element={<Wilayah />} />
-        <Route path="/bantuan" element={<Bantuan />} />
-        <Route path="/kalkulasi" element={<Kalkulasi />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:id" element={<Home />} />
+      <Route path="/katalog" element={<Katalog />} />
+      <Route path="/wilayah" element={<Wilayah />} />
+      <Route path="/bantuan" element={<Bantuan />} />
+      <Route path="/kalkulasi" element={<Kalkulasi />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import AppShell from "@components/molecules/AppShell/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppShell>
-      <App />
-    </AppShell>
+    <Router>
+      <AppShell>
+        <App />
+      </AppShell>
+    </Router>
   </React.StrictMode>
 );
